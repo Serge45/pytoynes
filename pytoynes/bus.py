97970cdba2cd07834c166c9709a54800f56f5140
Serge Lu <Serge45497@gmail.com>
2021-04-23 01:41:55 +0800

    cpu: fix incorrect value type in ram

--- pytoynes/bus.py
+++ pytoynes/bus.py
@@ -2,7 +2,7 @@ import array
 
 class Bus:
     def __init__(self):
-        self.ram = array.array('b', bytearray(64*1024))
+        self.ram = array.array('B', bytearray(64*1024))
 
     def write(self, addr, data):
         self.ram[addr] = data
